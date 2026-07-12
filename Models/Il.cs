168e2d56ab4885d96518c4323908db4ebb0b50c7
Julian <52328830+julianimpacto@users.noreply.github.com>
2026-07-12 08:53:40 -0500

    Agrego Il.cs

---
+++ Models/Il.cs
@@ -0,0 +1,21 @@
+public class Il
+{
+    Enter password:
+    public int Id { get; set; }
+    public string pr { get; set; }
+    public string cldeli { get; set; }
+    public string ftp { get; set; }
+    public int idcrac { get; set; }
+    public int idte { get; set; }
+    public DateOnly vi { get; set; }
+    public string mo { get; set; }
+    public string bada { get; set; }
+    public string lo { get; set; }
+    public int? nueq { get; set; }
+    public string eq { get; set; }
+    public string idso { get; set; }
+    public int? piso { get; set; }
+    public string idsepe { get; set; }
+    public string idsono { get; set; }
+    public int pisono { get; set; }
+}

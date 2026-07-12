71b860bd3f83c204f9230d1c6e91e7481f696688
Julian <52328830+julianimpacto@users.noreply.github.com>
2026-07-12 14:51:42 -0500

    Arreglando nulls

--- Models/Il.cs
+++ Models/Il.cs
@@ -7,19 +7,17 @@
     public int idcrac { get; set; }
     public int idte { get; set; }
 
-    // vi es DATE en MySQL → usar DateTime
     public DateTime vi { get; set; }
-
-    // mo es TIMESTAMP en MySQL → usar DateTime
     public DateTime mo { get; set; }
 
     public string bada { get; set; } = string.Empty;
     public string lo { get; set; } = string.Empty;
-    public int nueq { get; set; } = 1;
-    public string eq { get; set; } = string.Empty;
-    public string idso { get; set; } = string.Empty;
-    public int piso { get; set; } = 0;
-    public string idsepe { get; set; } = string.Empty;
+
+    public int? nueq { get; set; }   // puede ser NULL
+    public string? eq { get; set; }  // 👈 puede ser NULL
+    public string? idso { get; set; }
+    public int? piso { get; set; }
+    public string? idsepe { get; set; }
     public string idsono { get; set; } = string.Empty;
-    public int pisono { get; set; } = 0;
+    public int pisono { get; set; }
 }

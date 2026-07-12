0aae5de12778a7bfa165c92ecd0947d3c4a0f5ff
Julian <52328830+julianimpacto@users.noreply.github.com>
2026-07-12 09:13:47 -0500

    Reparo MyDbContext

--- MyDbContext.cs
+++ MyDbContext.cs
@@ -9,6 +9,6 @@ public class MyDbContext : DbContext {
     // Ejemplo: tabla de usuarios
     public DbSet<Us> Us { get; set; }
 
-    public DbSet<Us> Us { get; set; }
+    public DbSet<Il> Il { get; set; }
 }
 

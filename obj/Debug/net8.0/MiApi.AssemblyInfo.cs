2675a7b5dc9467d8f04b5f2007e74cd4cf386351
Julian <52328830+julianimpacto@users.noreply.github.com>
2026-07-14 10:22:08 -0500

    incluyendo librerias asp.net

--- obj/Debug/net8.0/MiApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+092b882b13f17aba9a2d976c56d0bf6d3371a8c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+292274bfbdfdc1c955c9834242b69c197dfff950")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

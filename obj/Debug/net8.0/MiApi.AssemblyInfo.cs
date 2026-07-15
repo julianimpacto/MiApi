7b4065f9254ed0830148d9aadd2cc4d59c2d34ba
Julian <52328830+julianimpacto@users.noreply.github.com>
2026-07-15 18:22:49 -0500

    Eliminado SqlDirectController.cs

--- obj/Debug/net8.0/MiApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1fec115457c1b2e323741844b2028efd6ff3ee9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc31d8338847150d56baf6240e276165e5b0fb94")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

b4ef375633b87e629e47212296068a4094528bd7
Julian <52328830+julianimpacto@users.noreply.github.com>
2026-07-15 19:13:56 -0500

    nuevo sqlcontgroller

--- Controllers/SQLController.cs
+++ Controllers/SQLController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using MiApi.Models;
+using System.Globalization;
 
 namespace MiApi.Controllers
 {
@@ -36,19 +37,10 @@ namespace MiApi.Controllers
                     var fila = new Dictionary<string, object>();
                     for (int i = 0; i < reader.FieldCount; i++)
                     {
-                        object valor = reader.GetValue(i);
-
-                        // 🔹 Normalización segura
-                        if (valor == DBNull.Value)
-                            fila[reader.GetName(i)] = null;
-                        else if (valor is int || valor is long || valor is short)
-                            fila[reader.GetName(i)] = Convert.ToInt32(valor);
-                        else if (valor is decimal || valor is double || valor is float)
-                            fila[reader.GetName(i)] = Convert.ToDouble(valor);
-                        else if (valor is bool b)
-                            fila[reader.GetName(i)] = b;
-                        else
-                            fila[reader.GetName(i)] = valor.ToString(); // todo lo demás como string
+                        // 🔹 Forzar todo a string o null
+                        fila[reader.GetName(i)] = reader.IsDBNull(i)
+                            ? null
+                            : Convert.ToString(reader.GetValue(i), CultureInfo.InvariantCulture);
                     }
                     resultados.Add(fila);
                 }
@@ -57,33 +49,10 @@ namespace MiApi.Controllers
             }
             catch (Exception ex)
             {
-                return StatusCode(500, new { Error = ex.Message });
+                return StatusCode(500, new { Error = ex.Message, StackTrace = ex.StackTrace });
             }
         }
 
         // UPDATE
         [HttpPut]
-        public IActionResult Update([FromBody] SqlRequest request) => EjecutarNonQuery(request.Query);
-
-        // DELETE
-        [HttpDelete]
-        public IActionResult Delete([FromBody] SqlRequest request) => EjecutarNonQuery(request.Query);
-
-        // 🔹 Método auxiliar para INSERT/UPDATE/DELETE
-        private IActionResult EjecutarNonQuery(string query)
-        {
-            try
-            {
-                using var conn = new MySqlConnection(_connectionString);
-                conn.Open();
-                using var cmd = new MySqlCommand(query, conn);
-                int filas = cmd.ExecuteNonQuery();
-                return Ok(new { FilasAfectadas = filas });
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new { Error = ex.Message });
-            }
-        }
-    }
-}
+        public IActionResult Update([FromBody] SqlRequest request
